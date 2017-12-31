07c07c3d78088bfb295f28d26e7a43fd1b7ad3c2
Lucas Benninger <benningerlucas@gmail.com>
2017-12-30 19:42:06 -0500

    Account-Check for nulls

--- app/routes/account_routes.js
+++ app/routes/account_routes.js
@@ -8,12 +8,15 @@ module.exports = function(app, db){
             password : req.body.password,
             firebase : req.body.firebase
         };
-        db.collection('accounts').insert(credentials, (err, result) =>{
-            if(err){
-                res.send({'error':'An Error Occured'});
-            }else{
-                res.send(result.ops[0]);
-            }
-        })
+        //Don't continue if nothing provided...
+        if(credentials._id != null && credentials.password != null){
+            db.collection('accounts').insert(credentials, (err, result) =>{
+                if(err){
+                    res.send({'error':'An Error Occured'});
+                }else{
+                    res.send(result.ops[0]);
+                }
+            });
+        }
     });
 }
